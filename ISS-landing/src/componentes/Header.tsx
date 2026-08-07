5cc7f8e73c35e00dcd90fc57552b614c35fcc7d8
Lirav <zlpi23818@gmail.com>
2026-08-07 14:03:46 -0500

    Header simple para empezar con el diseño de la página (#1)

--- ISS-landing/src/componentes/Header.tsx
+++ ISS-landing/src/componentes/Header.tsx
@@ -4,9 +4,16 @@ import "./Header.css";
 export function Header(){
     return (
         <header className="header-container">
-            <div className="header-content">
-                <Package />
+            <div className="header-content" style={{width: "20%"}}>
+                <Package size={40} color="#333"/>
                 <h1 className="header-title">ISS Soluciones</h1>
+            </div>
+            <div className="header-content" style={{width: "50%"}}>
+                <h2>Servicios</h2>
+                <h2>Inicio</h2>
+                <h2>Blog</h2>
+                <h2>Sobre Nosotros</h2>
+                
             </div>
         </header>
     )
