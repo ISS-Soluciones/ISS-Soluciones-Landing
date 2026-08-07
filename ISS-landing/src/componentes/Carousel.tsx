e1844a41a0fba36ae22550dede8f6f6fa8375ebe
just-zlpi <zlpi23818@gmail.com>
2026-08-07 17:34:09 -0500

    Implementado react-bootstrap

--- ISS-landing/src/componentes/Carousel.tsx
+++ ISS-landing/src/componentes/Carousel.tsx
@@ -3,27 +3,27 @@ import CarouselSlide from './Carousel-slide';
 
 export function HeroCarousel() {
   return (
-    <Carousel >
+    <Carousel fade >
       <Carousel.Item>
         <CarouselSlide imgSrc="src/assets/sample-hero.png" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <CarouselSlide imgSrc="src/assets/sample-hero.png" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h3>Foto principal</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <CarouselSlide imgSrc="src/assets/sample-hero.png" />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Secundario</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CarouselSlide imgSrc="src/assets/hero.png" />
+        <Carousel.Caption>
+          <h3>Tercero</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
